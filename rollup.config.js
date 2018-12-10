eb8280cf863e4b77008ef74909abe30beee6c214
Kevin Scott <kevin@hitheory.com>
2018-12-10 11:12:42 -0500

    Adding initial files

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from 'rollup-plugin-typescript';
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'esm',
+  },
+  plugins: [
+    typescript(),
+  ],
+  external: [
+    // 'vega-embed',
+    // 'emotion',
+  ],
+}
+
